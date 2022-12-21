37d17ec805e6a4fc8b68af85e7c9922726b0277e
jefersonviana <jefersonv28@gmail.com>
2022-12-21 17:20:39 -0300

    Requisito 2 concluído

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const body = document.querySelector('body');
 const input = document.createElement('input');
 input.id = 'carta-text';
+const paragraph = document.createElement('p');
+paragraph.id = 'carta-gerada';
