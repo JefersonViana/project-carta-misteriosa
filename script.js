01ef6c91bef8131418b01c7039329d2b526506b9
jefersonviana <jefersonv28@gmail.com>
2022-12-22 15:01:09 -0300

    Requisito 18 concluído

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const input = document.createElement('input');
 input.id = 'carta-texto';
 input.type = 'text';
 const paragraph = document.createElement('p');
+const paragraphContador = document.createElement('p');
+paragraphContador.id = 'carta-contador';
 const button = document.createElement('button');
 button.id = 'criar-carta';
 button.innerText = 'Sou o botão!';
@@ -10,15 +12,12 @@ paragraph.id = 'carta-gerada';
 body.appendChild(input);
 body.appendChild(button);
 body.appendChild(paragraph);
+body.appendChild(paragraphContador);
 
 const arrayEstilo = ['newspaper', 'magazine1', 'magazine2'];
 const arrayTamanho = ['medium', 'big', 'reallybig'];
 const arrayRotacao = ['rotateleft', 'rotateright'];
 const arrayInclinacao = ['skewleft', 'skewright'];
-console.log(arrayEstilo[Math.floor(Math.random() * 3)]);
-console.log(arrayTamanho[Math.floor(Math.random() * 3)]);
-console.log(arrayRotacao[Math.floor(Math.random() * 2)]);
-console.log(arrayInclinacao[Math.floor(Math.random() * 2)]);
 
 const funcao2 = () => {
   paragraph.innerText = 'Por favor, digite o conteúdo da carta.';
@@ -26,12 +25,12 @@ const funcao2 = () => {
 
 const funcao1 = () => {
   const texto = input.value;
-  console.log(texto.length);
   if (texto.length === 0 || texto[0] === ' ') {
     funcao2();
   } else {
     const arrayDePalavras = texto.split(' ');
     paragraph.innerHTML = '';
+    paragraphContador.innerText = arrayDePalavras.length;
     for (let index = 0; index < arrayDePalavras.length; index += 1) {
       const createdSpan = document.createElement('span');
       createdSpan.className = `${arrayEstilo[Math.floor(Math.random() * 3)]} ${arrayTamanho[Math.floor(Math.random() * 3)]} ${arrayRotacao[Math.floor(Math.random() * 2)]} ${arrayInclinacao[Math.floor(Math.random() * 2)]}`;
@@ -50,4 +49,3 @@ const funcao3 = (event) => {
 };
 
 paragraph.addEventListener('click', funcao3);
-
